13b6a8b4359f0d0d78f476fea443b78e326e49f8
daniman123 <daniel-saleh@hotmail.com>
2023-03-31 13:51:46 +0200

    url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <HashRouter basename="/arabic-to-english-learing-app">
     <App />
   </HashRouter>
 );
