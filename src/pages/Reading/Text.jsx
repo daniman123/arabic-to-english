2a91de30a68fe55ba5ea423a3eb07534f1b59f54
daniman123 <daniel-saleh@hotmail.com>
2023-04-05 18:25:55 +0200

    file struct

--- src/pages/Reading/Text.jsx
+++ src/pages/Reading/Text.jsx
@@ -1,18 +1,26 @@
-export default function Text({ text }) {
-    return (
-      <p className="text">
-        {text.split(" ").map((word, index) => (
-          <span key={index}>
-            <a
-              href={`https://translate.google.com/#en/ar/${word}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {word}
-            </a>{" "}
-          </span>
-        ))}
-      </p>
-    );
-  }
+function splitText(text) {
+  return text.split(" ");
+}
+
+function wrapWordsWithLink(words, getLink) {
+  return words.map((word, index) => (
+    <span key={index}>
+      <a href={getLink(word)} target="_blank" rel="noreferrer">
+        {word}
+      </a>{" "}
+    </span>
+  ));
+}
+
+function getDefaultLink(word) {
+  return `https://translate.google.com/#en/ar/${word}`;
+}
+
+export default function Text({ text, getLink = getDefaultLink }) {
+  const words = splitText(text);
+  const wordsWithLinks = wrapWordsWithLink(words, getLink);
+
+  return <p className="text">{wordsWithLinks}</p>;
+}
+
 
